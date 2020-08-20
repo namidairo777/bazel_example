86f21277d160b896292e45cc20a7366435b3df95
namidairo777 (github) <namidairo777@163.com>
2020-08-20 21:40:23 +0900

    First commit

---
+++ java_projects/src/main/java/com/xiao/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.xiao;
+
+import com.xiao.generator.Generator;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        final Generator g = new Generator();
+        System.out.println(g.getMessage());
+    }
+}
---
+++ java_projects/src/main/java/com/xiao/generator/Generator.java
@@ -0,0 +1,7 @@
+package com.xiao.generator;
+
+public class Generator {
+    public String getMessage() {
+        return "Hello world";
+    }
+}
